48176ef5107103ac6d35ec705c4f50c2c147e3b3
Jayyaah <valentines.maitre@gmail.com>
2022-12-06 12:32:20 +0100

    Initial Commit

---
+++ EasyTravel/EasyTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyTravelApp.swift
+//  EasyTravel
+//
+//  Created by Valentine on 06/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct EasyTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
